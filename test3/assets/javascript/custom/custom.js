931fd39810c5c7c5722b8ebf197df48d562ec018
eugene-kul <63741776+eugene-kul@users.noreply.github.com>
2020-05-12 21:46:38 +0500

    3

--- test3/assets/javascript/custom/custom.js
+++
@@ -1,8 +0,0 @@
-function ready() {
-    var elem = document.getElementById("currentPage");
-
-    if (elem != null) {
-        elem.innerText = (document.title.split(' | '))[1];
-    }
-}
-document.addEventListener("DOMContentLoaded", ready);
