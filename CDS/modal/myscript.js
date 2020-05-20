49db3e125a912a684a6b61460eb209e912bdef47
eugene-kul <63741776+eugene-kul@users.noreply.github.com>
2020-05-20 20:34:04 +0500

    modal.v3

--- CDS/modal/myscript.js
+++ CDS/modal/myscript.js
@@ -18,3 +18,32 @@ $('.rate-list__navtab-item').click(function(event){
 	$('.rate-list__tab-item.rate-list__tab-item_active').removeClass('rate-list__tab-item_active');
 	tab.addClass('rate-list__tab-item_active');
 });
+//==========================================================================================================
+//modal tabs для окна communication-v2
+$('.communication__navtab-item--v1').click(function(event){
+	if ($(this).hasClass('communication__navtab-item_active')) return;
+
+	var tabName = $(this).data('tab'),
+		tab = $('.communication__tabitem--v1[data-tab="'+tabName+'"]');
+
+	$('.communication__navtab-item--v1.communication__navtab-item_active').removeClass('communication__navtab-item_active');
+	$(this).addClass('communication__navtab-item_active');
+
+	$('.communication__tabitem--v1.communication__tabitem_active').removeClass('communication__tabitem_active');
+	tab.addClass('communication__tabitem_active');
+});
+
+// lkz jryf communication-v2
+$('.communication__navtab-item--v2').click(function(event){
+	if ($(this).hasClass('communication__navtab-item_active')) return;
+
+	var tabName = $(this).data('tab'),
+		tab = $('.communication__tabitem--v2[data-tab="'+tabName+'"]');
+
+	$('.communication__navtab-item--v2.communication__navtab-item_active').removeClass('communication__navtab-item_active');
+	$(this).addClass('communication__navtab-item_active');
+
+	$('.communication__tabitem--v2.communication__tabitem_active').removeClass('communication__tabitem_active');
+	tab.addClass('communication__tabitem_active');
+});
+//========================================================================================================================================================
