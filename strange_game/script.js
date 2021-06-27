0605502c93c1faeab3647da40e9eacced3a3dc34
eugene-kul <63741776+eugene-kul@users.noreply.github.com>
2021-06-27 18:05:30 +0500

    1

--- strange_game/script.js
+++ strange_game/script.js
@@ -14,3 +14,37 @@ for (let cell of _game.cells) {
 		}
 	});
 }
+
+let modal_btns = document.querySelectorAll('.js-open-modal');
+let modal_body = document.querySelector('.js-modal-body');
+let modal_overflow = modal_body.querySelector('.js-modal-overflow');
+for (let btn of modal_btns) {
+	btn.addEventListener('click', function(e){
+		e.preventDefault();
+		let modalId = this.dataset.open_modal;
+		let modalElem = document.querySelector('.js-modal[data-modal="' + modalId + '"]');
+
+		let modal_close = modalElem.querySelectorAll('[data-modal="close"]');
+		for (let item of modal_close) {
+			item.addEventListener('click', function(e){
+				e.preventDefault();
+				modal_body.classList.remove('active');
+				modalElem.classList.remove('active');
+				modal_overflow.classList.remove('active');
+				document.body.classList.remove('lock');
+			})
+		}
+		
+		modal_overflow.addEventListener('click', function(){
+			modal_body.classList.remove('active');
+			modalElem.classList.remove('active');
+			modal_overflow.classList.remove('active');
+			document.body.classList.remove('lock');
+		})
+		
+		modal_body.classList.add('active');
+		modalElem.classList.add('active');
+		modal_overflow.classList.add('active');
+		document.body.classList.add('lock');
+	})
+}
